c97c6dfb45a680cec545306da798c671afdceb21
3braa04 <braabormo@gmail.com>
2026-02-11 21:57:19 +0300

    fix dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './app/features/landing/landingPage';
 import LoginPage from './app/features/login/loginPage';
 import { ThemeProvider } from './components/theme-provider';
+import DashboardPage from './app/features/dashboard/dashboardPage';
+import { TooltipProvider } from './components/ui/tooltip';
+import { DirectionProvider } from './components/ui/direction';
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>      
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        
-        {/* 404 Route */}
-        <Route path="*" element={<h1>Page Not Found</h1>} />
-      </Routes>
-    </Router>
-    </ThemeProvider>
+    <TooltipProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Router>      
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage/>} />
+          
+          {/* 404 Route */}
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Router>
+      </ThemeProvider>
+    </TooltipProvider>
+    
   );
 }
 
