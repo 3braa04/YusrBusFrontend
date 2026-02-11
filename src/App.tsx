4e1ddcb9ecd5130f0918cd7817e956e6dc98fd24
3braa04 <braabormo@gmail.com>
2026-02-11 19:50:02 +0300

    adding landing, login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './app/features/landing/landingPage';
+import LoginPage from './app/features/login/loginPage';
+import { ThemeProvider } from './components/theme-provider';
 
-export function App() {
-return <h1>hhh</h1>;
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>      
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* 404 Route */}
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/app/features/landing/landingPage.tsx
@@ -0,0 +1,28 @@
+import { ModeToggle } from '@/components/mode-toggle';
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      
+      <div className="absolute top-5 right-5">
+        <ModeToggle />
+      </div>
+
+      <h1 className="text-4xl font-bold text-blue-600 dark:text-blue-400">
+        Welcome to YusrBus
+      </h1>
+      <p className="mt-4 text-gray-600 dark:text-gray-400">
+        Your journey, simplified.
+      </p>
+      
+      <Link to="/login">
+        <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 my-5 rounded-lg transition-all">
+          Login
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Landing;
