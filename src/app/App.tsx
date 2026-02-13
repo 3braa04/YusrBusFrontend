32f35e9469e453f09c3ed9a7111b544b6f035221
AhmedM204 <ahmedbkry204@gmail.com>
2026-02-13 15:54:23 +0300

    fix- restruct folders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import DashboardPage from "./app/features/dashboard/dashboardPage";
-import LandingPage from "./app/features/landing/landingPage";
-import LoginPage from "./app/features/login/loginPage";
-import { ThemeProvider } from "./app/core/components/theme/themeProvider";
-import { TooltipProvider } from "./components/ui/tooltip";
-import MainPage from "./app/features/main/mainPage";
-import Testing from "./app/test";
-import useAppInitialization from "./hooks/useAppInitialization";
-import { Skeleton } from "./components/ui/skeleton";
+import DashboardPage from "./features/dashboard/dashboardPage";
+import LandingPage from "./features/landing/landingPage";
+import LoginPage from "./features/login/loginPage";
+import { ThemeProvider } from "./core/components/theme/themeProvider";
+import { TooltipProvider } from "../components/ui/tooltip";
+import MainPage from "./features/main/mainPage";
+import Testing from "./test";
+import useAppInitialization from "../hooks/useAppInitialization";
+import { Skeleton } from "../components/ui/skeleton";
 
 function App() {
   const { isLoading } = useAppInitialization();
@@ -54,8 +54,6 @@ function Apploading() {
     <div className="flex justify-center items-center h-screen">
       <div className="flex w-full max-w-xs flex-col gap-2">
         <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-3/4" />
       </div>
     </div>
   );
