16ea11a60abae603f9586398f59d660f0015225f
AhmedM204 <ahmedbkry204@gmail.com>
2026-02-13 18:51:55 +0300

    enhance- make sidebar dynamic with app_text

--- src/app/core/components/sideBar/sideBar.tsx
+++ src/app/core/components/sideBar/sideBar.tsx
@@ -1,65 +1,75 @@
-import * as React from "react"
-import { SideBarSecondaryMenu } from "@/app/core/components/sideBar/sideBarSecondaryMenu"
-import { SideBarUserData } from "@/app/core/components/sideBar/sideBarUserData"
-import { 
-  LayoutDashboardIcon, 
-  BusFrontIcon,      
-  UsersIcon, 
-  MapPinnedIcon,     
-  Building2Icon,    
-  UserCogIcon,       
-  Settings2Icon, 
-  CommandIcon, 
-} from "lucide-react"
-import { SideBarMainMenu } from "./sideBarMainMenu"
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
-
+import * as React from "react";
+import { SideBarSecondaryMenu } from "@/app/core/components/sideBar/sideBarSecondaryMenu";
+import { SideBarUserData } from "@/app/core/components/sideBar/sideBarUserData";
+import {
+  LayoutDashboardIcon,
+  BusFrontIcon,
+  UsersIcon,
+  MapPinnedIcon,
+  Building2Icon,
+  UserCogIcon,
+  Settings2Icon,
+  CommandIcon,
+} from "lucide-react";
+import { SideBarMainMenu } from "./sideBarMainMenu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import ApplicationLang from "../../Services/LangService/ApplicationLang";
+const appLang = ApplicationLang.getAppLangText();  
+const appLangSections = appLang.sections;
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: "Baraa Barmo",
+    email: "Barrrr@gmail.com",
     avatar: "/avatars/shadcn.jpg",
   },
   navMain: [
     {
-      title: "لوحة المعلومات",
+      title: appLangSections.dashboard,
       url: "/dashboard",
       icon: <LayoutDashboardIcon />,
     },
     {
-      title: "الرحلات", 
+      title: appLangSections.trips,
       url: "/trips",
-      icon: <BusFrontIcon />, 
+      icon: <BusFrontIcon />,
     },
     {
-      title: "الركاب", 
+      title: appLangSections.passengers,
       url: "/passengers",
       icon: <UsersIcon />,
     },
     {
-      title: "الخطوط",
+      title: appLangSections.routes,
       url: "/routes",
-      icon: <MapPinnedIcon />, 
+      icon: <MapPinnedIcon />,
     },
     {
-      title: "الفروع", 
+      title: appLangSections.branches,
       url: "/branches",
       icon: <Building2Icon />,
     },
     {
-      title: "المستخدمون", 
+      title: appLangSections.users,
       url: "/users",
       icon: <UserCogIcon />,
     },
   ],
   navSecondary: [
     {
-      title: "الإعدادات",
+      title: appLangSections.settings,
       url: "/settings",
       icon: <Settings2Icon />,
     },
   ],
-}
+};
 
 export function SideBar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -73,7 +83,9 @@ export function SideBar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <CommandIcon className="size-5!" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <span className="text-base font-semibold">
+                  {appLang.companyName}
+                </span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -81,11 +93,14 @@ export function SideBar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <SideBarMainMenu items={data.navMain} />
-        <SideBarSecondaryMenu items={data.navSecondary} className="pt-10 mt-auto" />
+        <SideBarSecondaryMenu
+          items={data.navSecondary}
+          className="pt-10 mt-auto"
+        />
       </SidebarContent>
       <SidebarFooter>
         <SideBarUserData user={data.user} />
       </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
