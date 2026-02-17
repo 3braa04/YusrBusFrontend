662fbb7d5a454939118d4d2da955cb1a092b246c
3braa04 <braabormo@gmail.com>
2026-02-17 21:36:10 +0300

    fix names in useDialog

--- src/app/core/Hooks/useDialog.ts
+++ src/app/core/Hooks/useDialog.ts
@@ -2,20 +2,20 @@ import { useState } from "react";
 
 export default function useDialog<T>()
 {
-      const [activeBranch, setActiveBranch] = useState<T | null>(null);
+      const [activeEntity, setActiveEntity] = useState<T | null>(null);
       const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
       const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
     
-      const openEditDialog = (branch: T) => {
-        setActiveBranch(branch);
+      const openEditDialog = (entity: T) => {
+        setActiveEntity(entity);
         setIsEditDialogOpen(true);
       };
     
-      const openDeleteDialog = (branch: T) => {
-        setActiveBranch(branch);
+      const openDeleteDialog = (entity: T) => {
+        setActiveEntity(entity);
         setIsDeleteDialogOpen(true);
       };
 
 
-      return {activeBranch, isEditDialogOpen, isDeleteDialogOpen,setIsEditDialogOpen, setIsDeleteDialogOpen, openEditDialog, openDeleteDialog}
+      return {activeEntity, isEditDialogOpen, isDeleteDialogOpen,setIsEditDialogOpen, setIsDeleteDialogOpen, openEditDialog, openDeleteDialog}
 }
