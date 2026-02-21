60810d8104f4dedc7d8912f390546dc404eabc2e
AhmedM204 <ahmedbkry204@gmail.com>
2026-02-22 00:34:13 +0300

    complete- integrateion with dashboard

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import type { Dashboard } from "@/app/core/Data/Dashboard"
 import ApplicationLang from "@/app/core/Services/LangService/ApplicationLang"
 import { Badge } from "@/components/ui/badge"
 import {
@@ -11,7 +12,10 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { TrendingDownIcon, TrendingUpIcon } from "lucide-react"
-export function SectionCards() {
+type DashboardSectionCardsProps={
+  data:Dashboard
+}
+export function SectionCards({data}:DashboardSectionCardsProps) {
   const lang = ApplicationLang.getAppLangText().dashborad;
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
@@ -19,93 +23,33 @@ export function SectionCards() {
         <CardHeader>
           <CardDescription>{lang.income}</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
+            {data.totalIncome}
           </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <TrendingUpIcon
-              />
-              +12.5%
-            </Badge>
-          </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month{" "}
-            <TrendingUpIcon className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
-        </CardFooter>
       </Card>
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>{lang.totalCustomers}</CardDescription>
+          <CardDescription>{lang.totalPassengers}</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
+            {data.totalPassengers}
           </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <TrendingDownIcon
-              />
-              -20%
-            </Badge>
-          </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period{" "}
-            <TrendingDownIcon className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
-        </CardFooter>
       </Card>
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>{lang.routes}</CardDescription>
+          <CardDescription>{lang.totalTrips}</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
+            {data.totalTrips}
           </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <TrendingUpIcon
-              />
-              +12.5%
-            </Badge>
-          </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention{" "}
-            <TrendingUpIcon className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
-        </CardFooter>
       </Card>
       <Card className="@container/card">
         <CardHeader>
           <CardDescription>{lang.growthRate}</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
+            {data.grothRate}
           </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <TrendingUpIcon
-              />
-              +4.5%
-            </Badge>
-          </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase{" "}
-            <TrendingUpIcon className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
-        </CardFooter>
       </Card>
     </div>
   )
