31eb9e7104ccbbb9372d3e8e2e6a54c991499020
AhmedM204 <ahmedbkry204@gmail.com>
2026-02-24 00:54:37 +0300

    prepare- for users branches

--- src/app/features/users/data/user.ts
+++ src/app/features/users/data/user.ts
@@ -1,4 +1,5 @@
 import { BaseEntity } from "@/app/core/data/baseEntity";
+import type Branch from "../../branches/data/branch";
 
 export default class User extends BaseEntity
 {
@@ -6,6 +7,7 @@ export default class User extends BaseEntity
     public password! : string;
     public isActive! : boolean;
     public permissions! : number;
-
+    public branches!: Branch[];
+    
     constructor(init?: Partial<User>) { super(); Object.assign(this, init); }
 }
