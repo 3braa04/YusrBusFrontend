85e70fad634f484de86f0cf2f305d25dc8cb102a
AhmedM204 <ahmedbkry204@gmail.com>
2026-02-24 11:28:32 +0300

    fix- users model to be consistant with backend dto

--- src/app/features/users/data/user.ts
+++ src/app/features/users/data/user.ts
@@ -1,23 +1,22 @@
 import { BaseEntity } from "@/app/core/data/baseEntity";
-import type { ColumnName } from "@/app/core/types/ColumnName";
-import type Branch from "../../branches/data/branch";
 
-export default class User extends BaseEntity
-{
-    public username! : string;
-    public password! : string;
-    public isActive! : boolean;
-    public permissions! : number;
-    public branches!: Branch[];
-    
-    constructor(init?: Partial<User>) { super(); Object.assign(this, init); }
+export class UserBranchDto {
+  public userId!: number;
+  public branchId!: number;
+  constructor(init?: Partial<UserBranchDto>) {
+    Object.assign(this, init);
+  }
 }
 
-export class UserFilterColumns 
-{
-    public static columnsNames: ColumnName[] = [
-        { label: "اسم المستخدم", value: "Username" },
-        { label: "رقم المستخدم", value: "Id" }, 
-    ];
-}
+export default class User extends BaseEntity {
+  public username!: string;
+  public password!: string;
+  public isActive!: boolean;
+  public permissions!: number;
+  public branches!: UserBranchDto[];
 
+  constructor(init?: Partial<User>) {
+    super();
+    Object.assign(this, init);
+  }
+}
