f9d7d4166abe9e0084b3036969a7ad48fe81556f
3braa04 <braabormo@gmail.com>
2026-02-25 23:31:31 +0300

    add deposit

---
+++ src/app/features/trips/data/deposit.ts
@@ -0,0 +1,23 @@
+import { BaseEntity } from "@/app/core/data/baseEntity";
+import type { StorageFile } from "@/app/core/data/storageFile";
+
+export class deposit extends BaseEntity
+{
+    public tripId! : number;
+    public fromCityId! : number;
+    public toCityId! : number;
+    public sender! : string;
+    public recipient! : string;
+    public senderPhone! : string;
+    public recipientPhone! : string;
+    public description! : string;
+    public amount! : number;
+    public paidAmount? : number;
+    public notes? : string;
+    public image? : StorageFile;
+    
+    public fromCityName! : string;
+    public toCityName! : string;
+
+    constructor(init?: Partial<deposit>) { super(); Object.assign(this, init); }
+}
