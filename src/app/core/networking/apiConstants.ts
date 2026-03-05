f9fd419803f5c3c25331cabf3ad9200559624eb1
3braa04 <braabormo@gmail.com>
2026-03-05 16:31:39 +0300

    change api url to cloudflare

--- src/app/core/networking/apiConstants.ts
+++ src/app/core/networking/apiConstants.ts
@@ -1,5 +1,5 @@
 export default class ApiConstants
 {
-    static baseUrl:string = "https://yusrbus.runasp.net/api";
+    static baseUrl:string = "https://api.bus.yusrsys.com/api";
     // static baseUrl:string = "https://localhost:7226/api";
 }
